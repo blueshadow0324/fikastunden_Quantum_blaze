184b5a50b1c254ff4d799bf1d9cd7c24c8823249
william <thefoxarc0324@gmail.com>
2025-02-01 16:15:38 +0100

    x17 Crimson Eagle

--- crimson_eagle-6069a23cec6de04c2498a095a3423e49ac29579f/build/index.js
+++
@@ -1,7 +0,0 @@
-function showDeliveryInfo() {
-    document.getElementById('delivery-info').style.display = 'block';
-}
-
-function cancelPurchase() {
-    document.getElementById('delivery-info').style.display = 'none';
-}
